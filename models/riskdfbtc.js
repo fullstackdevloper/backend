9393f93eaedd2776cecb23a35aa16b812c294da1
Mukesh Kumar <upworkmukesh@gmail.com>
2022-12-06 19:03:27 +0530

    add models

---
+++ models/riskdfbtc.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class riskdfbtc extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  riskdfbtc.init({
+    closetime:DataTypes.STRING,
+    closeprice:DataTypes.STRING,
+    udpil:DataTypes.TEXT,
+    udpim:DataTypes.TEXT,
+    udpis:DataTypes.TEXT,
+    mbi:DataTypes.TEXT,
+    tci:DataTypes.TEXT,
+    tcicv:DataTypes.TEXT,
+    mdccv:DataTypes.TEXT,
+    mdcdp:DataTypes.TEXT,
+    volume:DataTypes.TEXT,
+    mc:DataTypes.TEXT,
+    cs:DataTypes.TEXT,
+    // aa:DataTypes.STRING,
+  },
+   {
+    sequelize,
+    timestamps: false,
+    tableName: 'riskdfbtc',
+    modelName: 'riskdfbtc',
+  });
+  return riskdfbtc;
+};
